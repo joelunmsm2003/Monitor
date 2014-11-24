7baab401d87e1c7cced5871534c78845d1a1baff
byte <joelunmsm@gmail.com>
2014-11-24 07:46:49 -0500

    commit

--- MonitorApp/views.py
+++ MonitorApp/views.py
@@ -10,12 +10,50 @@ from django.http import HttpResponse
 from MonitorApp.models import *
 import datetime
 
+def ticket_add(request):
+	id = request.user.id
+	ticket = Ticket.objects.filter(estado=1).order_by('-id')
+	x=User.objects.get(pk=id)
+	grupo =x.groups.get()
+	grupo=str(grupo)
+	username = request.user.username
+	tipos=Tipo.objects.all()
+	estado_name=str('Nuevos')
+	print estado_name
 
-def ticket(request):
+	if request.method == 'POST':
+
+
+		form = FormTicket(request.POST)
+		
+		username = request.user.username
+		asunto = request.POST['asunto']
+		tipo = request.POST['tipo']
+		descripcion=request.POST['descripcion']
+		
+		fecha_inicio = datetime.datetime.today()
+		#estado 1=Nuevo	2=Atendido 3=Prueba 4=Cerrado
+		#tipo 1=Incidencia 2=Requerimento
+
+
+		c=User.objects.get(pk=id).ticket_set.create(cliente=username,asunto=asunto,tipo_id=1,descripcion=descripcion,fecha_inicio=fecha_inicio,validado=0,estado_id=1)
+	
+		c.save()
+		return HttpResponseRedirect("/ticket/1")
+	else:
+		form = FormTicket()
+
+
+
+	
+	return render(request, 'home.html', {'estado_name':estado_name,'tipos':tipos,'form': form,'username':username,'ticket':ticket,'grupo':grupo})
+
+def ticket(request,estado):
 
 	id = request.user.id
-	ticket_pendiente = Ticket.objects.filter(estado=1).order_by('-id')
-	ticket_cerrado = Ticket.objects.filter(estado=4)
+
+	ticket = Ticket.objects.filter(estado=estado).order_by('-id')
+
 	x=User.objects.get(pk=id)
 	grupo =x.groups.get()
 	username = request.user.username
@@ -38,7 +76,7 @@ def ticket(request):
 		tipo = request.POST['tipo']
 		descripcion=request.POST['descripcion']
 		
-		fecha_inicio = datetime.date.today()
+		fecha_inicio = datetime.datetime.today()
 		#estado 1=Nuevo	2=Atendido 3=Prueba 4=Cerrado
 		#tipo 1=Incidencia 2=Requerimento
 
@@ -53,9 +91,18 @@ def ticket(request):
 	else:
 		form = FormTicket()
 
+	if str(estado)=='1': 
+		estado_name= 'Nuevos'
+	if str(estado)=='2': 
+		estado_name= 'Atendidos'
+	if str(estado)=='3': 
+		estado_name= 'En Prueba'
+	if str(estado)=='4': 
+		estado_name= 'Cerrados'
+
+
 	
-	
-	return render(request, 'home.html', {'tipos':tipos,'form': form,'username':username,'ticket_pendiente':ticket_pendiente,'ticket_cerrado':ticket_cerrado,'grupo':grupo,'grupo_flag':grupo_flag})
+	return render(request, 'home.html', {'estado_name':estado_name,'tipos':tipos,'form': form,'username':username,'ticket':ticket,'grupo':grupo,'grupo_flag':grupo_flag})
 
 def logeate(request):
  
@@ -100,7 +147,7 @@ def editar_ticket(request,id):
 		tipo = request.POST['tipo']
 		print tipo
 		descripcion=request.POST['descripcion']
-		fecha_inicio = datetime.date.today()
+		fecha_inicio = datetime.datetime.today()
 
 		ticket.asunto = asunto
 		ticket.tipo_id =tipo
@@ -132,14 +179,57 @@ def atender(request,id):
 		ticket.save()
 		
 		msj='El ticket '+ticket.asunto+' esta siendo atendido'
-		return render(request, 'home.html', {'username':username,'grupo':grupo,'tipos':tipos,'msj':msj,'ticket_pendiente':ticket_pendiente})
-		
+		#return render(request, 'home.html', {'username':username,'grupo':grupo,'tipos':tipos,'msj':msj,'ticket':ticket_pendiente})
+		return HttpResponseRedirect("/ticket/2")
 
 	if ticket.estado_id == 2:
 		msj = ''
-		
-		return render(request, 'home.html', {'username':username,'grupo':grupo,'tipos':tipos,'msj':msj,'ticket_pendiente':ticket_pendiente})
+		return HttpResponseRedirect("/ticket/2")
+		#return render(request, 'home.html', {'username':username,'grupo':grupo,'tipos':tipos,'msj':msj,'ticket':ticket_pendiente})
 
 
 
-	return HttpResponseRedirect("/ticket")
+	return HttpResponseRedirect("/ticket/2")
+
+def cerrar(request,id):
+
+	ticket= Ticket.objects.get(id=id)
+	ticket.estado_id = 3
+	ticket.save()
+
+	return HttpResponseRedirect("/ticket/3")
+
+
+def reasignar(request,id):
+
+	ticket= Ticket.objects.get(id=id)
+	username = request.user.username
+	tipos=Tipo.objects.all()
+	x=User.objects.get(username=username)
+	soporte = User.objects.all()
+	grupo =x.groups.get()
+	grupo= str(grupo)
+	return render(request, 'reasignar.html', {'soporte':soporte,'username':username,'grupo':grupo,'tipos':tipos,'ticket':ticket})
+
+
+
+def ver_ticket(request,id):
+
+	ticket= Ticket.objects.get(id=id)
+	username = request.user.username
+	tipos=Tipo.objects.all()
+	x=User.objects.get(username=username)
+	grupo =x.groups.get()
+	grupo= str(grupo)
+
+
+	return render(request, 'detalle.html', {'username':username,'grupo':grupo,'tipos':tipos,'ticket':ticket})
+
+
+def validar(request,id):
+
+	ticket= Ticket.objects.get(id=id)
+	ticket.estado_id = 4
+	ticket.save()
+
+	return HttpResponseRedirect("/ticket/1")
